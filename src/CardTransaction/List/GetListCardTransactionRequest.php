6d281b07aa483f864e8b0a913b0309414f1829c7
Friends of Vertex Cards <friendsofvertexcards@gmail.com>
2025-10-15 00:40:56 +0400

    Feat/card transaction query params (#3)
    * feat: add card transaction query params
    
    * feat: add phpunit cmd

--- src/CardTransaction/List/GetListCardTransactionRequest.php
+++ src/CardTransaction/List/GetListCardTransactionRequest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace FriendsOfVertexCards\ApiClient\CardTransaction\List;
 
+use FriendsOfVertexCards\ApiClient\Enum\CardTransactionSortBy;
+use FriendsOfVertexCards\ApiClient\Enum\CardTransactionType;
+use FriendsOfVertexCards\ApiClient\Enum\SortType;
 use FriendsOfVertexCards\ApiClient\RequestInterface;
 use Ramsey\Uuid\UuidInterface;
 
@@ -12,12 +15,28 @@ use Ramsey\Uuid\UuidInterface;
  */
 final class GetListCardTransactionRequest implements RequestInterface
 {
+    private const string DATETIME_FORMAT = 'Y-m-d\TH:i:s.v\Z';
+
+    /**
+     * @param UuidInterface[]       $ids
+     * @param UuidInterface[]       $accountIds
+     * @param UuidInterface[]       $cardIds
+     * @param CardTransactionType[] $types
+     */
     public function __construct(
         private readonly UuidInterface $companyId,
         public int $page,
         public int $quantity,
         public ?UuidInterface $accountId = null,
         public ?UuidInterface $cardId = null,
+        public array $ids = [],
+        public array $accountIds = [],
+        public array $cardIds = [],
+        public array $types = [],
+        public ?\DateTimeInterface $createdAtFrom = null,
+        public ?\DateTimeInterface $createdAtTo = null,
+        public SortType $sortType = SortType::Desc,
+        public CardTransactionSortBy $sortBy = CardTransactionSortBy::CreatedAt,
     ) {}
 
     public function getPath(): string
@@ -32,12 +51,25 @@ final class GetListCardTransactionRequest implements RequestInterface
 
     public function getQueryParams(): array
     {
+        if ($this->accountId !== null) {
+            $this->accountIds[] = $this->accountId;
+        }
+        if ($this->cardId !== null) {
+            $this->cardIds[] = $this->cardId;
+        }
+
         return array_filter(
             [
                 'page' => $this->page,
                 'quantity' => $this->quantity,
-                'accountId' => $this->accountId?->toString(),
-                'cardId' => $this->cardId?->toString(),
+                'accountIds' => array_map(static fn (UuidInterface $id) => $id->toString(), $this->accountIds),
+                'cardIds' => array_map(static fn (UuidInterface $id) => $id->toString(), $this->cardIds),
+                'ids' => array_map(static fn (UuidInterface $id) => $id->toString(), $this->ids),
+                'types' => array_map(static fn (CardTransactionType $id) => $id->value, $this->types),
+                'createdAtFrom' => $this->createdAtFrom?->format(self::DATETIME_FORMAT),
+                'createdAtTo' => $this->createdAtTo?->format(self::DATETIME_FORMAT),
+                'sortType' => $this->sortType->value,
+                'sortBy' => $this->sortBy->value,
             ],
         );
     }
