354c529432a680f5426c610a7a89d25324f72ee2
friends-of-vertex-cards <friendsofvertexcards@gmail.com>
2025-10-17 01:12:06 +0400

    feat: topup\withdraw card balance use cases

---
+++ examples/withdraw-card-balance.php
@@ -0,0 +1,70 @@
+<?php
+
+use FriendsOfVertexCards\ApiClient\Account\GetAccountById\GetAccountByIdRequest;
+use FriendsOfVertexCards\ApiClient\Card\Details\CardDetailsRequest;
+use FriendsOfVertexCards\ApiClient\Card\WithdrawBalance\WithdrawCardRequest;
+use FriendsOfVertexCards\ApiClient\Dto\ConfigureDto;
+use FriendsOfVertexCards\ApiClient\VertexCardsClientFactory;
+use Http\Adapter\Guzzle7\Client;
+use Psr\Log\NullLogger;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+$serializer = new Serializer(
+    [
+        new ObjectNormalizer(),
+    ],
+    [
+        new JsonEncoder(),
+    ],
+);
+
+$apiKey = 'your-api-key';
+$companyId = Uuid::fromString('your-company-id');
+$accountId = Uuid::fromString('your-account-id');
+$cardId = Uuid::fromString('your-card-id');
+$amount = '1';
+
+$factory = new VertexCardsClientFactory(
+    new Client(),
+    $serializer,
+    'https://gateway.vertex-cards.com',
+    '/api/v1/cards',
+    new NullLogger(),
+);
+
+$client = $factory->create(new ConfigureDto($apiKey));
+
+$account = $client->getAccount(
+    new GetAccountByIdRequest(
+        $companyId,
+        $accountId,
+    ),
+);
+
+if (!$account->data->isUsedCardBalance) {
+    throw new RuntimeException('Card balance is not used. All cards use account balance');
+}
+
+$card = $client->getCard(
+    new CardDetailsRequest(
+        $companyId,
+        $accountId,
+        $cardId,
+    ),
+);
+
+if (bccomp($card->data->card->availableBalance ?? '0', $amount, 2) === -1) {
+    throw new RuntimeException('Insufficient balance');
+}
+
+$response = $client->withdrawCardBalance(
+    new WithdrawCardRequest(
+        $companyId,
+        $accountId,
+        $cardId,
+        $amount,
+    ),
+);
